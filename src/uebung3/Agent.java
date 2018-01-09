eeba58476fb58a0588c671a616312dc49c4090a2
Christopher Baumann <goriar.cb@gmail.com>
2018-01-09 03:21:34 +0100

    first working

---
+++ src/uebung3/Agent.java
@@ -0,0 +1,16 @@
+package uebung3;
+
+import repast.simphony.space.continuous.NdPoint;
+
+public interface Agent {
+
+	public int getId();
+
+	public int getStartX();
+
+	public int getStartY();
+
+	public NdPoint getStartingPoint();
+
+	public void setId(int id);
+}
--- src/uebung3/MessageCenter.java
+++ src/uebung3/MessageCenter.java
@@ -1,15 +1,47 @@
 package uebung3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MessageCenter {
 
 	ArrayList<FIPA_Message> messageList;
+	HashMap<Integer, Agent> agents;
 
+	public int numberOfAgents;
+	public int numberOfMessengers;
+	public int numberOfClients;
+
+	public static final MessageCenter singleton = new MessageCenter();
 	// Das MessageCenter dient als Plattform zum Nachrichtenaustausch.
 
 	public MessageCenter() {
 		this.messageList = new ArrayList<FIPA_Message>();
+		this.agents = new HashMap<>();
+		this.numberOfAgents = 0;
+	}
+
+	public void requestPackage(Client c) {
+		for (int i = 0; i < agents.size(); i++) {
+			Agent a = agents.get(i);
+			if (a instanceof Messenger) {
+				send(c.getId(), a.getId(), FIPA_Performative.REQUEST, "cfp");
+			}
+		}
+	}
+
+	public Agent getAgent(int id) {
+		return agents.get(id);
+	}
+
+	public void addAgent(Agent a) {
+		a.setId(numberOfAgents);
+		this.agents.put(numberOfAgents++, a);
+		if (a instanceof Messenger) {
+			numberOfMessengers++;
+		} else {
+			numberOfClients++;
+		}
 	}
 
 	public void addMessage(FIPA_Message message) {
